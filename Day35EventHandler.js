b5cfa400ecdcd0d842346875081d8d3974be83b7
Md-Naim-Hassan-Borson <naimhassanborson10102@gamil.com>
2023-09-25 23:15:08 +0600

    fifth commit

---
+++ Day35EventHandler.js
@@ -0,0 +1,36 @@
+function myMassage(){
+    alert("Welcome to my world! It's Test massage1 bro..");
+}
+
+function myMassage1(){
+    alert("Welcome to my world! It's Test massage2 bro..");
+}
+
+
+function paragraph(){
+    alert("Welcome to my world! It's Test massage3 bro..");
+}
+
+
+
+
+var myText = document.getElementById("para");
+// var myText = document.querySelector("#para");
+function myMassage2(){
+    myText.innerHTML = "you have clicked on Navigate Button";
+}
+
+function myMassage3(){
+    myText.innerHTML = "you have clicked on Care Button";
+}
+
+
+
+var MyImage = document.querySelector("#imageId");
+function image(){
+    MyImage.src = "Images/pic4.jpg";
+}
+
+function image1(){
+    MyImage.src = "Images/pic1.jpg";
+}
