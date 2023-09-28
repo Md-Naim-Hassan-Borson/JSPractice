9bb9634930757606f87762025df4dd668472dbdf
Md-Naim-Hassan-Borson <naimhassanborson10102@gamil.com>
2023-09-29 02:27:43 +0600

    9th session

---
+++ Day38ImageSlider.js
@@ -0,0 +1,31 @@
+
+var photos = ["Images/pic1.jpg","Images/pic2.jpg", "Images/pic3.jpg", "Images/pic4.jpg", "Images/pic5.jpg", "Images/pic6.jpg"];
+
+var img = document.querySelector("img");
+
+
+count = 0;
+
+
+function next(){
+    count++;
+    if(count >= photos.length){
+        count = 0;
+        img.src = photos[count];
+    }else{
+        img.src = photos[count];
+    }
+    
+
+} 
+
+function prev(){
+    count--;
+    if(count< 0){
+        count = photos.length-1;
+        img.src = photos[count];
+    }else{
+        img.src = photos[count];
+    }
+
+}
